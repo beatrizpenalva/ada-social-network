74afb6a637ab920b660e1553626e8186f44d094f
Beatriz Penalva <biapenalva@gmail.com>
2021-01-09 17:07:51 -0300

    revisão de código

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,15 @@
 import { onNavigate } from '../../utils/history.js';
 import { signInEmail, signUpGoogle, signUpFacebook, signUpGitHub } from '../../services/index.js'
+
 export const Login = () => {
-  const rootElement = document.createElement("div");
+  const rootElement = document.createElement("main");
   rootElement.innerHTML = `
     <section class="login-page">  
       <section class="left">  
         <img src="../../img/ada-lovelace.svg" width="175px" height="175px">
         <p class="logo-theme"><span class="logo-name">[Ada]</span> Programe como uma mulher.</p>
       </section>
+      
       <section class="right">
         <form id="signin-form">
           <fieldset class="right">
@@ -15,27 +17,33 @@ export const Login = () => {
             <input type="password" id="password" class="input-in-line" placeholder="Senha" required>
             <section id="error-login" class="error-message"></section>
           </fieldset>
+          
           <fieldset class="login-button">
             <button type="submit" class="enter-button">Entrar</button> 
           </fieldset>
-        </form>    
+        </form> 
+
         <p class="subtitle">_______________ OU _______________</p>
+        
         <section class="providers-buttons">
           <button id="google-button" class="button-icon"><img src="../../img/google.svg" height="50px" width="50px"></button>
           <button id="facebook-button" class="button-icon"><img src="../../img/facebook.svg" height="50px" width="50px"></button>
           <button id="github-button" class="button-icon"><img src="../../img/github.svg" height="50px" width="50px"></button>
         </section>  
+
         <p class="subtitle">Não tem uma conta?
           <button id="button-register" class="button-in-text">Registre-se</button>
         </p>
       </section>
     </section>    
   `;
+
   const signInButton = rootElement.querySelector("#signin-form");
   const goRegisterButton = rootElement.querySelector("#button-register");
   const signUpGoogleButton = rootElement.querySelector("#google-button");
   const signUpFbButton = rootElement.querySelector("#facebook-button");
   const signUpGhButton = rootElement.querySelector("#github-button");
+  
   signInButton.addEventListener("submit", (event) => {
     event.preventDefault();
     const email = rootElement.querySelector("#email").value;
@@ -49,5 +57,6 @@ export const Login = () => {
     event.preventDefault();
     onNavigate('/');
   });
+
   return rootElement;
 };
