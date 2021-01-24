3572e4086c58a43dda73eb2b9f6f2fbcee813837
Beatriz Penalva <biapenalva@gmail.com>
2021-01-24 10:08:19 -0300

    função de comentar nos próprios posts

--- src/components/posts.js
+++ src/components/posts.js
@@ -43,49 +43,52 @@ export const printPosts = (doc, id, currentUser) => {
   } else {
     postContainer.innerHTML = `
       <section class="post-container" id="${id}">
-        <section class="left-post">
+        <section class="top-post">
+          <section class="left-post">
             <img class="avatar" src="${post.avatar}" height="60px" width="60px">
-        </section>
+          </section>
 
-        <section class="right-post">
+          <section class="right-post">
             <article class="user-info">
-                <h4 class="username">${post.name}</h4>
-                <p class="post-date">${post.date}</p>
+              <h4 class="username">${post.name}</h4>
+              <p class="post-date">${post.date}</p>
             </article>
 
             <article class="post-content">${post.text}</article>
 
             <section class="post-buttons">
-                <button class="post-function edit" id="edit-${id}">
-                    <figure>
-                        <img src="../../img/edit.png" height="20px" width="20px">
-                    </figure>
-                </button>
+              <button class="post-function edit" id="edit-${id}">
+                <img src="../../img/edit.png" height="20px" width="20px">
+              </button>
 
-                <button class="post-function delete" id="delete-${id}">
-                    <figure>
-                        <img src="../../img/delete.png" height="20px" width="20px">
-                    </figure>
-                </button>
+              <button class="post-function delete" id="delete-${id}">
+                <img src="../../img/delete.png" height="20px" width="20px">
+              </button>
             </section>
 
             <form id="edit-post-form-${id}" class="on-edition">
-                <textarea class="edition-content text" id="edition-content-${id}" spellcheck="true" maxlength="500"
-                    wrap="hard" required>${post.text}</textarea>
-
+              <textarea class="edition-content text" id="edition-content-${id}" spellcheck="true" maxlength="500" wrap="hard" required>${post.text}</textarea>
                 <fieldset class="edition-buttons">
-                    <button class="post-function cancel-edition" id="cancel-edition-${id}">
-                        <figure>
-                            <img src="../../img/cancel.png" height="20px" width="20px">
-                        </figure>
-                      </button>    
+                  <button class="post-function cancel-edition" id="cancel-edition-${id}">
+                    <img src="../../img/cancel.png" height="20px" width="20px">
+                  </button>    
 
-                    <button type="submit" class="send-button">Salvar</button>
+                  <button type="submit" class="send-button">Salvar</button>
                 </fieldset>
             </form>
+          </section>  
+        </section>
+
+        <section class="bottom-post">
+          <section id="comment-${id}" class="comment-container">
+            <section id="new-comment-${id}" class="form-comment">
+              <textarea id="commentText-${id}" class="comment-text" spellcheck="true" maxlength="250" wrap="hard" placeholder="Adicione seu comentário." required></textarea>
+              <button class="comment-button">Publicar</button> 
+            </section>
+          </section>
         </section>
       </section>
-       `;
+     `;
   }
 
   const deleteButtons = postContainer.querySelectorAll('.delete');
@@ -130,11 +133,11 @@ const sendComment = (e) => {
       const commentId = res.id;
       commentBox.prepend(createCommentBox(newComment, commentId, newComment.userID))
       commentText.value = ""
-      //não deixar postar sem nada
-      //rever ordem em que os comentários são printados, aqui é o inverso: os mais novos ficam embaixo
-      //esconder os comentários - esconder os botões dos comentários de acordo com quem postou (like e editar)
-      //ajustar o CSS do post em si - tá com bug
-      //refatorar o código
+      //não deixar postar sem nada 1
+      //rever ordem em que os comentários são printados, aqui é o inverso: os mais novos ficam embaixo 3
+      //ajustar o CSS do post em si - tá com bug 4
+      //refatorar o código 5
+      //esconder os comentários - esconder os botões dos comentários de acordo com quem postou (like e editar) 6
       //const commentForm = document.querySelector(`new-comment-${postId}`)
       //commentBox.insertBefore(createCommentBox(newComment), commentForm)
     })
