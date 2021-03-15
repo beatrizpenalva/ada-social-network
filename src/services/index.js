9b7a8ae0513a14845ea8426973392290a9817a2c
Beatriz Penalva <biapenalva@gmail.com>
2021-03-15 10:43:20 -0300

    login com github funcionando

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,12 @@
-import { getError } from '../errors/index.js';
+import { getError } from "../errors/index.js";
 
-export const verifyUserLogged = (callback) => firebase.auth().onAuthStateChanged(callback);
+export const verifyUserLogged = (callback) =>
+  firebase.auth().onAuthStateChanged(callback);
 
 export const getCurrentUser = () => firebase.auth().currentUser;
 
-export const signInEmail = (email, password) => firebase.auth().signInWithEmailAndPassword(email, password);
+export const signInEmail = (email, password) =>
+  firebase.auth().signInWithEmailAndPassword(email, password);
 
 export const getGoogleProvider = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider();
@@ -22,8 +24,12 @@ export const getGitHubProvider = () => {
 };
 
 const singUpProvider = (provider) => {
-  firebase.auth().signInWithPopup(provider)
-    .then(() => {
+  provider.addScope("user");
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((user) => {
+      saveUserProfile(user.user);
     })
     .catch((err) => {
       getError(err);
@@ -33,34 +39,72 @@ const singUpProvider = (provider) => {
 export const logOut = () => firebase.auth().signOut();
 
 export const saveUserProfile = (userName) => {
-  const user = getCurrentUser()
-  user.updateProfile({displayName: userName});
-}
+  const user = getCurrentUser();
+  user.updateProfile({ displayName: userName });
+};
 
-export const recordNewUser = (userEmail, userPassword) => firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword);
+export const recordNewUser = (userEmail, userPassword) =>
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword);
 
-export const getPosts = () => firebase.firestore().collection('posts').orderBy('time', 'desc').get();
+export const getPosts = () =>
+  firebase.firestore().collection("posts").orderBy("time", "desc").get();
 
-export const createNewPost = (post) => firebase.firestore().collection('posts').add(post);
+export const createNewPost = (post) =>
+  firebase.firestore().collection("posts").add(post);
 
-export const editPost = (postID, newPostText) => firebase.firestore().collection('posts').doc(postID).update({ text: newPostText });
+export const editPost = (postID, newPostText) =>
+  firebase
+    .firestore()
+    .collection("posts")
+    .doc(postID)
+    .update({ text: newPostText });
 
-export const deletePost = (postID) => firebase.firestore().collection('posts').doc(postID).delete();
+export const deletePost = (postID) =>
+  firebase.firestore().collection("posts").doc(postID).delete();
 
-export const alreadyLikedThisPost = (postID) => firebase.firestore().collection('posts').doc(postID).get();
+export const alreadyLikedThisPost = (postID) =>
+  firebase.firestore().collection("posts").doc(postID).get();
 
-export const removeLike = (postID, userID) => firebase.firestore().collection('posts').doc(postID).update({ likes: firebase.firestore.FieldValue.arrayRemove(userID) });
+export const removeLike = (postID, userID) =>
+  firebase
+    .firestore()
+    .collection("posts")
+    .doc(postID)
+    .update({ likes: firebase.firestore.FieldValue.arrayRemove(userID) });
 
-export const likePost = (postID, userID) => firebase.firestore().collection('posts').doc(postID).update({ likes: firebase.firestore.FieldValue.arrayUnion(userID) });
+export const likePost = (postID, userID) =>
+  firebase
+    .firestore()
+    .collection("posts")
+    .doc(postID)
+    .update({ likes: firebase.firestore.FieldValue.arrayUnion(userID) });
 
-export const addComment = (commentObject) => firebase.firestore().collection('comments').add(commentObject);
+export const addComment = (commentObject) =>
+  firebase.firestore().collection("comments").add(commentObject);
 
-export const getCommentsById = (postId) => firebase.firestore().collection('comments').where("postId", "==", postId).get();
+export const getCommentsById = (postId) =>
+  firebase
+    .firestore()
+    .collection("comments")
+    .where("postId", "==", postId)
+    .get();
 
-export const deleteComment = (commentID) => firebase.firestore().collection('comments').doc(commentID).delete();
+export const deleteComment = (commentID) =>
+  firebase.firestore().collection("comments").doc(commentID).delete();
 
-export const alreadyLikedThisComment = (commentID) => firebase.firestore().collection('comments').doc(commentID).get();
+export const alreadyLikedThisComment = (commentID) =>
+  firebase.firestore().collection("comments").doc(commentID).get();
 
-export const removeLikeComment = (commentID, userID) => firebase.firestore().collection('comments').doc(commentID).update({ likes: firebase.firestore.FieldValue.arrayRemove(userID) });
+export const removeLikeComment = (commentID, userID) =>
+  firebase
+    .firestore()
+    .collection("comments")
+    .doc(commentID)
+    .update({ likes: firebase.firestore.FieldValue.arrayRemove(userID) });
 
-export const likeComment = (commentID, userID) => firebase.firestore().collection('comments').doc(commentID).update({ likes: firebase.firestore.FieldValue.arrayUnion(userID) });
+export const likeComment = (commentID, userID) =>
+  firebase
+    .firestore()
+    .collection("comments")
+    .doc(commentID)
+    .update({ likes: firebase.firestore.FieldValue.arrayUnion(userID) });
