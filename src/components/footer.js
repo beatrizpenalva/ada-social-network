91155c309150633858889def18091def916e7d6d
Beatriz Penalva <biapenalva@gmail.com>
2021-03-19 16:27:56 -0300

    footer com info sobre as devas

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+export const createFooter = () => {
+    const footerContainer = document.createElement('footer');
+    footerContainer.innerHTML = `
+      <section class="footer">
+        <p>
+            <i class="fab fa-github-alt footer-icon"></i>
+            Desenvolvido por
+            <a class="devas" href="https://github.com/beatrizpenalva" target="blank" title="GITHUB PROFILE">Beatriz Penalva</a> 
+            ,
+            <a class="devas" href="https://github.com/beatrizpenalva" target="blank" title="GITHUB PROFILE">Gabrielle Almeida</a>
+            e 
+            <a class="devas" href="https://github.com/beatrizpenalva" target="blank" title="GITHUB PROFILE">Julia Terin</a>
+            . 
+        </p>
+      </section>
+    `;
+    return footerContainer;
+  };
